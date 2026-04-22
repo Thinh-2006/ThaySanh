d93a3cd7391f7e14598a5564ea08faec823360bf
daika <lamnguyentienthinh@gmail.com>
2026-04-22 08:09:59 +0700

    lan dau

---
+++ src/thuchanhh/ThucHanhh.java
@@ -0,0 +1,13 @@
+
+package thuchanhh;
+
+
+public class ThucHanhh {
+
+    
+    public static void main(String[] args) {
+         JFThuchanh c = new JFThuchanh ();
+        c.show();
+    }
+    
+}
